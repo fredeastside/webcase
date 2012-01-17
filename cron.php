f4e8c66912c28129d3adcbad9d62926618362ebd
fredrsf <fredrsf@yandex.ru>
2012-01-17 23:15:32 +0400

    17.01.2012 -2

---
+++ cron.php
@@ -0,0 +1,42 @@
+<?php
+	require_once 'Models/M_SQL.php';
+	require_once 'Models/M_News.php';
+	require_once 'Models/M_Users.php';
+	require_once 'Controllers/simple_html_dom.php';
+	require_once 'Controllers/C_Phpmailer.php';
+		
+		$mNews = M_News::Instance();
+		$mUsers = M_Users::Instance();
+		$title_last_new = $mNews->SelectTitleLastNew();
+		
+        $html_content_links = file_get_html('http://soft.mail.ru/pressrl_list.php?cat=15');
+
+        foreach($html_content_links->find('p.news-title a') as $element)
+        {
+            $html_content = file_get_html('http://soft.mail.ru/' . $element->href);
+
+            foreach($html_content->find('h1.article-header') as $e)
+                $title = $e->innertext;
+				
+			if(iconv('windows-1251', 'utf-8', $title) === $title_last_new)
+				break;
+
+            foreach($html_content->find('p.article-announce') as $e)
+                $content = '<p>' . $e->innertext . '</p>';
+
+            foreach($html_content->find('div.article-body') as $e)
+                $content .= $e->innertext;
+
+            break;
+        }
+		
+		//echo iconv('windows-1251', 'utf-8', $title) . '<br/>';
+		//echo $title . '<br/>';
+		//echo iconv('windows-1251', 'utf-8', $content);
+		//echo $content;
+		
+		if($content)
+		{
+			$mNews->AddNew(iconv('windows-1251', 'utf-8', $title), date('Y-m-d H:i:s'), '<a href="http://soft.mail.ru/pressrl_list.php?cat=15">News for developers</a>', iconv('windows-1251', 'utf-8', $content));
+		}
+?>
